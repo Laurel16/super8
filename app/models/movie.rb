90b67eb8e47e04dd51e337f3bfda34d1c33cabef
Laure Leter <laure.leter@gmail.com>
2018-04-30 14:41:17 +0200

    adding a movie category

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-  CATEGORIES = %w(Aventure Animation Enquête Politique Science-fiction Comédie)
-  AGES = %w(5 6 7 8 9 10 12 15)
+  CATEGORIES = %w(Aventure Animation Drame Enquête Politique Science-fiction Comédie)
+  AGES = %w(4 5 6 7 8 9 10 12 15)
    mount_uploader :picture, PhotoUploader
   mount_uploader :poster, PhotoUploader
    is_impressionable counter_cache: true, :unique => true
