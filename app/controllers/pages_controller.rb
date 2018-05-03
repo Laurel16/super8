e19606e9198e92d1c4305b5c2b4c22c75322c9b2
Laure Leter <laure.leter@gmail.com>
2018-05-03 13:15:30 +0200

    prepare footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @fresh_movies = Movie.order('greatest(created_at, updated_at) desc').limit(6)
+    @fresh_movies = Movie.order('greatest(created_at, updated_at) desc').limit(9)
+  end
+
+  def contact
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'pages/home'
+  get 'contact', to: 'pages#contact', as: :contact
+   get 'crédits', to: 'pages#credits', as: :credits
 
   devise_for :users
   root to: 'pages#home'
