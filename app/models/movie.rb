bce511ddc8f285268bb394ba71c93ca93c056970
Laure Leter <laure.leter@gmail.com>
2018-05-11 15:42:52 +0200

    adding a category

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  CATEGORIES = %w(Aventure Animation Drame Enquête Politique Science-fiction Comédie)
+  CATEGORIES = %w(Aventure Animation Drame Thriller Politique Science-fiction Comédie)
   AGES = %w(4 5 6 7 8 9 10 12 15)
    mount_uploader :picture, PhotoUploader
   mount_uploader :poster, PhotoUploader
