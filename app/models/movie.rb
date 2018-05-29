292e9766e4fb46b816cc109f19bbbb11025defd9
Laure Leter <laure.leter@gmail.com>
2018-05-29 14:25:49 +0200

    pb crash on heroku

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  CATEGORIES = %w(Comédie Aventure Animation Drame Thriller  Science-fiction Fantastique Guerre Musicale)"
+  CATEGORIES = %w(Comédie Aventure Animation Drame Thriller Science-fiction Fantastique Guerre Musicale)
 
   AGES = %w(4 5 6 7 8 9 10 11 12 13 14 15)
    mount_uploader :picture, PhotoUploader
